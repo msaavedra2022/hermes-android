5f9baafe349f960817041a44ce8d80b87fbb9f2d
rusty4444 <rusty4444@users.noreply.github.com>
2026-06-08 04:01:44 +1000

    Add release signing config with stable keystore; bump to v1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,15 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
    id("com.android.application")
    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePath = rootProject.projectDir.parentFile.resolve("key.properties")
+if (keystorePath.exists()) {
+   keystoreProperties.load(FileInputStream(keystorePath))
 }
 
 android {
@@ -21,9 +30,20 @@ android {
        versionName = flutter.versionName
    }
 
+   signingConfigs {
+       create("release") {
+           if (keystoreProperties.containsKey("storeFile")) {
+               storeFile = file(keystoreProperties["storeFile"] as String)
+               storePassword = keystoreProperties["storePassword"] as String
+               keyAlias = keystoreProperties["keyAlias"] as String
+               keyPassword = keystoreProperties["keyPassword"] as String
+           }
+       }
+   }
+
    buildTypes {
        release {
-           signingConfig = signingConfigs.getByName("debug")
+           signingConfig = signingConfigs.getByName("release")
        }
    }
 }
